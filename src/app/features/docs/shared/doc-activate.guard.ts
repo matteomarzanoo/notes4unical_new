a8b5def9be4413c1a5dc1f4b80f9c16c46eb1fde
Matteo Marzano <132601331+matteomarzanoo@users.noreply.github.com>
2025-05-30 22:14:49 +0200

    aaa

--- src/app/features/docs/shared/doc-activate.guard.ts
+++ src/app/features/docs/shared/doc-activate.guard.ts
@@ -5,30 +5,5 @@ import { UserRole } from '../../../core/auth/model/user-role';
 import { firstValueFrom } from 'rxjs';
 
 export const docActivateGuard: CanActivateFn = (route, state) => {
-  const authService = inject(AuthService);
-  const router = inject(Router);
-
-  const expectedRole:UserRole[] = route.data['requiredRoles']
-
-  return firstValueFrom(authService.getUser())
-    .then(user => {
-      if (!user) {
-        router.navigate(['/login'])
-      }
-
-      if (user) {
-        if (!expectedRole) {
-          return true;
-        }
-
-        if (expectedRole[1] == user.authorities[0].authority) {
-          return true;
-        }
-
-        return false;
-      }
-
-      return false;
-    })
-    .catch(_ => router.navigate(['/login']))
+  return true;
 };
