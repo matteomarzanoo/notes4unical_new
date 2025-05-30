338c594e473bb348687fca45f2bbe63d6f60686e
Matteo Marzano <132601331+matteomarzanoo@users.noreply.github.com>
2025-05-30 18:18:41 +0200

    2/4 of Doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 import { HeaderComponent } from './core/layout/header/header.component';
 import { FooterComponent } from './core/layout/footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
