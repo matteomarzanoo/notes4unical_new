69dbabf92d0fef2d5bf47ae8f816f8d24776136f
Marika <marikafoti2017@gmail.com>
2025-05-30 22:33:07 +0200

    aggiunta rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { HomeComponent } from './features/home/home.component';
 import { FeedbackComponent } from './shared/feedback/feedback.component';
+import { RegisterComponent } from './core/auth/components/register/register.component';
+import { LoginComponent } from './core/auth/components/login/login.component';
 
 export const routes: Routes = [
     {
@@ -9,6 +11,16 @@ export const routes: Routes = [
         title: 'Homepage | Notes4Unical - Be the Community',
         component: HomeComponent,
     },
+
+    { 
+        path: '/login',
+        component: LoginComponent,
+    },
+        { 
+        path: '/register',
+        component: RegisterComponent,
+    },
+
     {
         path: 'docs',
         loadChildren: () => import('./features/docs/docs.routes').then(r => r.docRoutes)
