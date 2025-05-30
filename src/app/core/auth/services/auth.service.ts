d1ae0278ce025b124d2cd188c0d2fa3661cc2422
Matteo Marzano <132601331+matteomarzanoo@users.noreply.github.com>
2025-05-30 22:13:06 +0200

    mini fix

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -65,27 +65,4 @@ logout(): void {
   isAdmin(): boolean {
     return localStorage.getItem('isAdmin') === 'true';
   }
-
-  getUser(): Observable<any | null> {
-    if (this.currentUserSubject.value) {
-      // Se l'utente è già presente, restituisci il valore
-      return of(this.currentUserSubject.value);
-    }
-
-    // Altrimenti, effettua una chiamata HTTP per recuperare l'utente
-    return this.http.get<any>(`/api/auth/v1/check-user`, {
-      withCredentials: true,
-    }).pipe(
-      switchMap((user) => {
-        // Se l'utente è autenticato, aggiorna il BehaviorSubject
-        this.currentUserSubject.next(user);
-        return of(user);
-      }),
-      catchError(() => {
-        // In caso di errore (es: 401 Unauthorized), restituisci null
-        this.currentUserSubject.next(null);
-        return of(null);
-      })
-    );
-  }
 }
